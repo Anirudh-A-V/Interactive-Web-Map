f9a44a90c6e07e278955afcbac5c8be4e9ceb9c8
Anirudh A V <76550448+Anirudh-A-V@users.noreply.github.com>
2022-05-23 15:01:42 +0530

    f

--- map1.py
+++ map1.py
@@ -1,13 +1,13 @@
 import folium
 import pandas as pd
 
-dams = pd.read_csv("Dams.csv")
+dams = pd.read_csv("dam.csv")
 
 map = folium.Map(location=[23.036829466930804, 79.0364234261668], zoom_start=5)
 
 fg = folium.FeatureGroup(name="My map")
-for x in dams:
-    fg.add_child(folium.Marker(location=[ float(x[1]), float(x[2])], popup=x[4], icon=folium.Icon(color='blue')))
+for x in range(15):
+    fg.add_child(folium.Marker(location=[ dams['Longitude'][x], dams['Latitude'][x]],icon=folium.Icon(color='blue')))
 # fg.add_child(folium.Marker(location=[ 8.546818512885398, 76.90552676576516], popup="College front", icon=folium.Icon(color='blue')))
 # fg.add_child(folium.Marker(location=[ 8.542525203744514, 76.90451714249187], popup="Students Shelter", icon=folium.Icon(color='blue')))
 map.add_child(fg)
