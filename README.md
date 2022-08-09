2dad0890410f5249beec42ba1dc05cbe38d4c397
cs-association <anirudhpchalla3@gmail.com>
2022-08-09 22:41:33 +0530

    Successfully completed the dam location plotting """ Next Rainfall gauging using API calls using a weather API. """

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Interactive-Population-map-kerala
 
+An Interactive map of all major dams of India along with the population data using Folium
+
+## How to use
+
+1. Install Folium and Pandas to your system.
+    `pip install pandas
+    `pip install folium
+
+2. Run the file map1.py
+    In Windows :
+        `python map1.py
+    In Linux :
+        `py map1.py
+
+3. Open the .html file (i.e. Map1.html) in your web browser
+
+
--- map1.py
+++ map1.py
@@ -1,15 +1,13 @@
 import folium
 import pandas as pd
 
-dams = pd.read_csv("dam.csv")
+dams = pd.read_csv("dam.csv",encoding='latin1')
 
 map = folium.Map(location=[23.036829466930804, 79.0364234261668], zoom_start=5)
 
-fg = folium.FeatureGroup(name="My map")
-for x in range(15):
-    fg.add_child(folium.Marker(location=[ dams['Longitude'][x], dams['Latitude'][x]],icon=folium.Icon(color='blue')))
-# fg.add_child(folium.Marker(location=[ 8.546818512885398, 76.90552676576516], popup="College front", icon=folium.Icon(color='blue')))
-# fg.add_child(folium.Marker(location=[ 8.542525203744514, 76.90451714249187], popup="Students Shelter", icon=folium.Icon(color='blue')))
-map.add_child(fg)
+# map.add_child(folium.Marker(location=[23.036829466930804, 79.0364234261668], popup="My Home", icon=folium.Icon(color='red')))
+
+for i in range(dams.shape[0]):
+    map.add_child(folium.Marker(location=[dams['Latitude'][i], dams['Longitude'][i]], popup=dams['dm_name'][i], icon=folium.Icon(color='blue')))
 
 map.save("Map1.html")
