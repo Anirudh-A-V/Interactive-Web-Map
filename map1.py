87113ff034a9e0941ac608cd32bd9f82498854da
Anirudh A V <76550448+Anirudh-A-V@users.noreply.github.com>
2022-08-10 16:20:40 +0530

    Update map1.py

--- map1.py
+++ map1.py
@@ -10,4 +10,4 @@ map = folium.Map(location=[23.036829466930804, 79.0364234261668], zoom_start=5)
 for i in range(dams.shape[0]):
     map.add_child(folium.Marker(location=[dams['Latitude'][i], dams['Longitude'][i]], popup=dams['dm_name'][i], icon=folium.Icon(color='blue')))
 
-map.save("Map1.html")
+map.save("index.html")
