08705e0b9cf4d3a663f9f7099cca66af065fd0e8
Anirudh A V <anirudh.av02@gmail.com>
2022-09-17 01:49:54 +0530

    Files reorganised > Choropelth removed > Geopandas package deprecated > Pyogorio package added

--- map.py
+++ map.py
@@ -2,39 +2,38 @@ import folium
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+import pyogrio as pyg
 
+dams = pd.read_csv("./data/dam.csv", encoding="latin1")
+rainfall = pd.read_csv("./data/rainfall.csv", encoding="latin1")
 
-dams = pd.read_csv("dam.csv", encoding="latin1")
-rainfall = pd.read_csv("rainfall.csv", encoding="latin1")
-
-geoJSON_df = gpd.read_file("states.geojson")
-geoJSON_df.head()
-
-geoJSON_states = list(geoJSON_df.NAME_1.values)
-
-no_0f_staes = len(geoJSON_states)
 
 map = folium.Map(location=[23.036829466930804, 79.0364234261668], zoom_start=5)
 
-folium.Choropleth(
-    geo_data=geoJSON_df,
-    data=geoJSON_df,
-    columns=["NAME_1", "geometry"],
-    key_on="feature.properties.NAME_1",
-    fill_color="YlGnBu",
-    fill_opacity=1,
-    line_opacity=0.2,
-    legend_name="wills",
-    smooth_factor=0,
-    Highlight=True,
-    line_color="#0000",
-    name="Rainfall",
-    show=False,
-    overlay=True,
-    nan_fill_color="White",
-)
+# geoJSON_df = gpd.read_file("states.geojson")
+# geoJSON_df.head()
 
-# map.add_child(folium.Marker(location=[23.036829466930804, 79.0364234261668], popup="My Home", icon=folium.Icon(color='red')))
+# geoJSON_states = list(geoJSON_df.NAME_1.values)
+
+# no_0f_staes = len(geoJSON_states)
+
+# folium.Choropleth(
+#     geo_data=geoJSON_df,
+#     data=geoJSON_df,
+#     columns=["NAME_1", "geometry"],
+#     key_on="feature.properties.NAME_1",
+#     fill_color="YlGnBu",
+#     fill_opacity=1,
+#     line_opacity=0.2,
+#     legend_name="wills",
+#     smooth_factor=0,
+#     Highlight=True,
+#     line_color="#0000",
+#     name="Rainfall",
+#     show=False,
+#     overlay=True,
+#     nan_fill_color="White",
+# )
 
 for i in range(dams.shape[0]):
     map.add_child(
