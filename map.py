179691d4cafafb7eef86842a90038d868a8e7984
Anirudh A V <anirudh.av02@gmail.com>
2022-09-23 00:05:38 +0530

    map.py updated

--- map.py
+++ map.py
@@ -10,31 +10,6 @@ rainfall = pd.read_csv("./data/rainfall.csv", encoding="latin1")
 
 map = folium.Map(location=[23.036829466930804, 79.0364234261668], zoom_start=5)
 
-# geoJSON_df = gpd.read_file("states.geojson")
-# geoJSON_df.head()
-
-# geoJSON_states = list(geoJSON_df.NAME_1.values)
-
-# no_0f_staes = len(geoJSON_states)
-
-# folium.Choropleth(
-#     geo_data=geoJSON_df,
-#     data=geoJSON_df,
-#     columns=["NAME_1", "geometry"],
-#     key_on="feature.properties.NAME_1",
-#     fill_color="YlGnBu",
-#     fill_opacity=1,
-#     line_opacity=0.2,
-#     legend_name="wills",
-#     smooth_factor=0,
-#     Highlight=True,
-#     line_color="#0000",
-#     name="Rainfall",
-#     show=False,
-#     overlay=True,
-#     nan_fill_color="White",
-# )
-
 for i in range(dams.shape[0]):
     map.add_child(
         folium.Marker(
@@ -45,3 +20,4 @@ for i in range(dams.shape[0]):
     )
 
 map.save("index.html")
+
